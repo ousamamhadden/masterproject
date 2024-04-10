943bd2b2630dd868d5c4432f2547d2ad199b7b05
aleksanderlj <aleksanderlj@hotmail.com>
2024-04-10 16:33:03 +0200

    Images.zip added to dvc

---
+++ job_script.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+### General options
+### –- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J testjob
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 1:00
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=5GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s185118@dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
+
+nvidia-smi
+# Load the cuda module
+module load cuda/11.6
+
+/appl/cuda/11.6.0/samples/bin/x86_64/linux/release/deviceQuery
+
+echo "Starting executable..."
+
+echo "Running: conda init and script"
+eval "$(conda shell.bash hook)" && conda activate src && make train
+
